af339fa95983f167cbb8c685c16b0f421da68ba9
FlorianHugCH <florian.hug@bs.ch>
2021-05-27 13:05:08 +0200

    join table cascade on delete

--- src/main/resources/sql/person_language_base.sql
+++ src/main/resources/sql/person_language_base.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS tbl_person_language(
     person_id bigint not null,
     language_id bigint not null,
     CONSTRAINT person_lang_pk PRIMARY KEY (person_id, language_id),
-    CONSTRAINT person_fk FOREIGN KEY (person_id) REFERENCES tbl_person (id),
-    CONSTRAINT language_fk FOREIGN KEY (language_id) REFERENCES tbl_language (id)
+    CONSTRAINT person_fk FOREIGN KEY (person_id) REFERENCES tbl_person (id) ON DELETE cascade,
+    CONSTRAINT language_fk FOREIGN KEY (language_id) REFERENCES tbl_language (id) ON DELETE cascade
 );
 
 INSERT INTO tbl_person_language (person_id, language_id) VALUES (
