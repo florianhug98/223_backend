266ecaf15239b5179dac4a24dcc4b1a2b25d4a2b
FlorianHugCH <florian.hug@bs.ch>
2021-05-27 10:28:03 +0200

    Language annotation added + mariadb drive added

--- src/main/java/ch/bbzbl/m223_backend/persistence/entity/Language.java
+++ src/main/java/ch/bbzbl/m223_backend/persistence/entity/Language.java
@@ -1,7 +1,39 @@
 package ch.bbzbl.m223_backend.persistence.entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
-@Entity
+@Entity(name = "tbl_language")
 public class Language {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @Column(name = "iso_code")
+    private String isoCode;
+
+    public long getId () {
+        return id;
+    }
+
+    public void setId (long id) {
+        this.id = id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getIsoCode () {
+        return isoCode;
+    }
+
+    public void setIsoCode (String isoCode) {
+        this.isoCode = isoCode;
+    }
 }
